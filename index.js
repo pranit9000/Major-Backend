dde9ece78b9f7464d59c2721e1c2aa3e4d4cc8c9
Pranit9000 <pranitsingh111@gmail.com>
2024-05-05 21:34:28 +0530

    Cors Error

--- index.js
+++ index.js
@@ -6,6 +6,12 @@ import cors from "cors";
 config({ path: "./config/config.env" });
 export const app = express();
 app.use(cors());
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', process.env.FRONTEND_URL);
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
